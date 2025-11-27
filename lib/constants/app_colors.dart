3472fac6121a04353f78ba1c973141c158580e43
IamWaqasMuhammad <waqas.developer5254@gmail.com>
2025-11-27 20:29:09 +0500

    add colors

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+// Foundation - Primary
+  static const Color primary = Color(0xFF0A5EFE);
+  static const Color primary80 = Color(0xFF0240B4);
+  static const Color primary95 = Color(0xFF01246E);
+  static const Color primary100 = Color(0xFF000D36);
+
+// Foundation - Secondary
+  static const Color secondary = Color(0xFF00D0B4);
+  static const Color secondary60 = Color(0xFF00A58F);
+  static const Color secondary70 = Color(0xFF007D6B);
+  static const Color secondary90 = Color(0xFF00332A);
+
+// Neutral
+  static const Color neutral0 = Color(0xFF0A5EFE);
+  static const Color neutral262626 = Color(0xFF262626);
+  static const Color neutral868686 = Color(0xFF868686);
+  static const Color neutralCECECE = Color(0xFFCECECE);
+  static const Color neutralF5F5F7 = Color(0xFFF5F5F7);
+  static const Color neutralFFFFFF = Color(0xFFFFFFFF);
+
+// Status
+  static const Color error = Color(0xFFE42C2C);
+  static const Color success = Color(0xFF40D336);
+  static const Color warning = Color(0xFFFFC03F);
+
+// Dark Mode - Foundation
+  static const Color darkPrimary = Color(0xFF2C74FF);
+  static const Color darkSecondary = Color(0xFF00D0B4);
+  static const Color darkBackground = Color(0xFF262626);
+  static const Color darkContent = Color(0xFFF5F5F7);
+}
